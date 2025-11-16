8f22c2f696a85aea3124d78cba218a50371223c6
nithinkp1 <131663803+nithinkp1@users.noreply.github.com>
2025-11-16 20:54:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+let count = 0;
 function insert_Row() {
-    //Write your code here
+	count++;
+            const table1 = document.getElementById("sampleTable")
+            
+            const tr = document.createElement("tr")
+
+            const td1 = document.createElement("td")
+            td1.innerText = `New cell ${count}`
+            count++;
+
+            const td2 = document.createElement("td")
+            td2.innerText = `New cell ${count}`
+
+            tr.append(td1,td2)
+
+            table1.prepend(tr)
+	
+    
   
   
 }
