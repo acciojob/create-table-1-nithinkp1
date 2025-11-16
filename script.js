cf81b3612ab9dbdffbd9d9c2e573fc1b1de2d1f4
nithinkp1 <131663803+nithinkp1@users.noreply.github.com>
2025-11-16 21:06:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,22 @@
-let count = 0;
+// let count = 0;
 function insert_Row() {
-	count++;
+	// count++;
             const table1 = document.getElementById("sampleTable")
             
             const tr = document.createElement("tr")
 
             const td1 = document.createElement("td")
-            td1.innerText = `New Cell${count}`
-            count++;
+            td1.innerText = `New Cell1`
+            // count++;
 
             const td2 = document.createElement("td")
-            td2.innerText = `New Cell${count}`
+            td2.innerText = `New Cell2`
 
             tr.append(td1,td2)
 
             table1.prepend(tr)
 	
-    it('inserts a new row at top', () => {
-  cy.get('#insertBtn').click();   // whatever triggers insert_Row()
-
-  cy.get('#sampleTable tr')
-    .first()
-    .within(() => {
-      cy.get('td').eq(0).should('have.text', 'New Cell1');
-      cy.get('td').eq(1).should('have.text', 'New Cell2');
-    });
-});
-
+    
   
   
 }
