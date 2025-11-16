413ae19fd0e409c2e80ddc9d30829f3df68eed9f
nithinkp1 <131663803+nithinkp1@users.noreply.github.com>
2025-11-16 20:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function insert_Row() {
             const tr = document.createElement("tr")
 
             const td1 = document.createElement("td")
-            td1.innerText = `New cell ${count}`
+            td1.innerText = `New Cell${count}`
             count++;
 
             const td2 = document.createElement("td")
-            td2.innerText = `New cell ${count}`
+            td2.innerText = `New Cell${count}`
 
             tr.append(td1,td2)
 
